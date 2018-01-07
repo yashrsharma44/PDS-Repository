0237139158e2a5879b669eca6bc0b70bcfc56e74
Yash <yash@email.com>
2018-01-07 11:11:06 +0530

    Modified cards.c to have count function

--- files/cards.c
+++ files/cards.c
@@ -32,6 +32,12 @@ int main()
 	{
 		val = atoi(card_name);
 	}
-	printf("The card value is :%i\n", val);
+
+	/* Check if the value is 3 to 6*/
+	if ((val>2)&&(val,7))
+		puts("Count has gone up");
+	else if(val == 10)
+		puts("Count has gone down");
+
 	return 0;
 }
